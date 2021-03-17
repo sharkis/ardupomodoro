3cf45640c367fb649bb916db93c8d4ae5ad11316
Albert Sharkis <sharkis@sharkis.org>
2021-03-17 12:41:31 -0600

    clang-format

--- ardupomodoro/ardupomodoro.ino
+++ ardupomodoro/ardupomodoro.ino
@@ -12,56 +12,53 @@ void setup() {
   arduboy.begin();
   arduboy.clear();
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   unsigned long curTime = millis();
   int hours = elapsed / 1000 / 60 / 60;
-  int minutes = (elapsed - hours * 3600000) /1000/60;
-  float seconds = (elapsed - (hours *3600000) - (minutes*60000))/1000.0;
+  int minutes = (elapsed - hours * 3600000) / 1000 / 60;
+  float seconds = (elapsed - (hours * 3600000) - (minutes * 60000)) / 1000.0;
   // press a to stop/start
   arduboy.pollButtons();
 
-  if(arduboy.justPressed(A_BUTTON)){
+  if (arduboy.justPressed(A_BUTTON)) {
     running = !running;
     start = curTime;
-    if(onBreak){
-      elapsed+=1;
-      onBreak=false;
+    if (onBreak) {
+      elapsed += 1;
+      onBreak = false;
     }
-
   }
-  if(arduboy.justPressed(B_BUTTON)){
-    elapsed =0;
-    hours=0;
-    minutes=0;
-    seconds=0.0;
-    running=false;
+  if (arduboy.justPressed(B_BUTTON)) {
+    elapsed = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0.0;
+    running = false;
   }
-  if(running){
+  if (running) {
     elapsed += curTime - start;
     start = curTime;
   }
-  
-  
+
   arduboy.clear();
-  arduboy.setCursor(0,0);
+  arduboy.setCursor(0, 0);
   arduboy.print(F("Elapsed: "));
   arduboy.print(hours);
   arduboy.print(F(":"));
   arduboy.print(minutes);
   arduboy.print(F(":"));
-  arduboy.println(seconds,3);
+  arduboy.println(seconds, 3);
   arduboy.print(F("Pomodoros: "));
   render_pomodoros(elapsed);
   arduboy.display();
   // put your main code here, to run repeatedly:
 }
 
-void render_pomodoros(unsigned long duration){
+void render_pomodoros(unsigned long duration) {
   int pomodoros = duration / POMODORO_LENGTH;
-  for(int i = 0; i < pomodoros; i++){
+  for (int i = 0; i < pomodoros; i++) {
     arduboy.print(F("*"));
   }
 }
